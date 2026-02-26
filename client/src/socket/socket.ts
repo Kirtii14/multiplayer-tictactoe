5ca2e6517c1fd84bb529c54a96afd5280a4a2a5b
Kirtii14 <kirtit1444@gmail.com>
2026-02-27 00:39:28 +0530

    fix port for vercel  deploy

--- client/src/socket/socket.ts
+++ client/src/socket/socket.ts
@@ -1,5 +1,10 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000", {
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://multiplayer-tictactoe-j3gv.onrender.com"
+    : "http://localhost:4000";
+
+export const socket = io(URL, {
   autoConnect: false,
 });
